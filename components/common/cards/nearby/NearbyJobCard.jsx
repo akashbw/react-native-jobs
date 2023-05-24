a7a30e3ae0ef36007218500e2eb96825a3aed425
akashbw <akashjain0108@gmail.com>
2023-05-24 22:54:50 +0800

    minor refactoring, added defaultImageLogo in utils

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -1,10 +1,8 @@
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 
 import styles from './nearbyjobcard.style';
-import { checkImageURL } from '../../../../utils';
+import { checkImageURL, defaultLogoImage } from '../../../../utils';
 
-const defaultLogoImage =
-  'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg';
 
 const NearbyJobCard = ({ job, handleNavigate }) => {
   return (
--- components/common/cards/popular/PopularJobCard.jsx
+++ components/common/cards/popular/PopularJobCard.jsx
@@ -2,10 +2,7 @@ import React from 'react';
 import { View, Text, TouchableOpacity, Image } from 'react-native';
 
 import styles from './popularjobcard.style';
-import { checkImageURL } from '../../../../utils';
-
-const defaultLogoImage =
-  'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg';
+import { checkImageURL, defaultLogoImage } from '../../../../utils';
 
 const PopularJobCard = ({ item, selectedJob, handleCardPress }) => {
   return (
